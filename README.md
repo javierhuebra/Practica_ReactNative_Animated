f273ec4d73a225cdc2e96085ea08da34aaa73af7
javierhuebra <javieremanuelhuebra@gmail.com>
2023-05-14 14:28:29 -0300

    changes

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Prácticas de animaciones en react native
 <div>
 <img src="/src/images/ada1.PNG"/>
 <img src="/src/images/ada2.PNG"/>
+<img src="/src/images/img3.PNG"/>
 </div>
--- src/components/Animacion7.jsx
+++ src/components/Animacion7.jsx
@@ -4,69 +4,81 @@ import { StyleSheet, View, Animated, Text, Pressable } from 'react-native'
 
 const Animacion7 = () => {
 
-    const [ animacion1 ] = useState( new Animated.Value(0))
-    const [ animacion2 ] = useState( new Animated.Value(1))
-    
-    
+    const [animacion1] = useState(new Animated.Value(0))
+    const [animacion2] = useState(new Animated.Value(1))
 
+    const resetear = () => {
+        animacion1.setValue(0)
+        animacion2.setValue(1)
+
+    }
+
+
+
+
+
+    const empezar = () => {
+        Animated.sequence([  //ACA PUEDO USAR .parallel para hacer todas las animaciones al mismo tiempo
+            Animated.timing(animacion1, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: false
+            }),
+            Animated.spring(animacion2, {
+                toValue: 4,
+                friction: 1,
+                tension: 30,
+                useNativeDriver: false
+            }),
+
+        ]).start()
+    }
 
     useEffect(() => {
-        Animated.loop(    // aca uso de nuevo loop para repetir infinitamente la sequence 
-        Animated.sequence([  //ACA PUEDO USAR .parallel para hacer todas las animaciones al mismo tiempo
-            Animated.timing( animacion1, {
-                toValue: 1,
-                duration: 1000,
-                useNativeDriver: false
-            }),
-            Animated.spring( animacion2, {
-                toValue: 4,
-                useNativeDriver: false
-            }),
-            Animated.spring( animacion2, {
-                toValue: 1,
-                useNativeDriver: false
-            }),
-            Animated.timing( animacion1, {
-                toValue: 0,
-                duration: 1000,
-                useNativeDriver: false
-            }),
-        ])).start()
-    },[])
+        empezar()
+    }, [])
 
     const estiloAnimacion = {
         transform: [
-            
+
             { scale: animacion2 }
         ],
         opacity: animacion1
     }
 
-    
+
 
     return (
         <View style={styles.cont}>
             <Animated.View
-                style={[styles.caja,estiloAnimacion]}
+                style={[styles.caja, estiloAnimacion]}
             >
 
             </Animated.View>
+
+            <Pressable onPress={() => resetear()}>
+                <Text>Reset Animation</Text>
+            </Pressable>
+            <Pressable onPress={() => empezar()}>
+                <Text>Start Animation</Text>
+            </Pressable>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-  caja: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'purple'
-  },
-  cont:{
-    backgroundColor: '#FFF',
-    height: 500,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    caja: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'purple'
+    },
+    cont: {
+        backgroundColor: '#FFF',
+        height: 500,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 
 })
 
